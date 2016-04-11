3c05bcb3418526c91b9d2c5a913ffa788a9a2bf7
Albert Chan <atfchan@gmail.com>
2016-04-12 01:04:34 +0800

    Modified containers/components to import granularized styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { translate } from 'react-i18next';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Footer.scss';
 
-function Footer() {
+function Footer(props) {
+    const { t } = props;
+
     return (
-        <div className={s.footer}>
-            Footer
+        <div className={s.root}>
+            <div className={s.container}>
+                <div className={s.copyright}>{ t('footer.copyright') }</div>
+            </div>
         </div>
     );
 }
 
-export default withStyles(Footer, s);
+
+export default translate(['common'])(withStyles(Footer, s));
